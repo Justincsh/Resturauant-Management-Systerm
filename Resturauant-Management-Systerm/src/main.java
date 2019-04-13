4d5cb57d131e8bcc5cf8b74c74a127bd3502eec2
“Justincsh” <justincsh@outlook.com>
2019-04-13 05:21:55 -0700

    create main.java

---
+++ Resturauant-Management-Systerm/src/main.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author justinchan
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
