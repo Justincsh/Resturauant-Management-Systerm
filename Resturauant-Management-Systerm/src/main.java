094ab6bbb0d70d6f28ca92ff25e04e9829c974ca
“Justincsh” <justincsh@outlook.com>
2019-04-13 05:25:22 -0700

    create main.java

--- Resturauant-Management-Systerm/src/main.java
+++ Resturauant-Management-Systerm/src/main.java
@@ -1,9 +1,10 @@
-
-
-/**
- *
- * @author justinchan
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+
+
 public class main {
 
     /**
